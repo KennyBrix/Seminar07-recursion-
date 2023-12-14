893368da7586de897cdfc7c55bc8aa1090265233
Alex <kenny480rus@mail.ru>
2023-12-14 13:12:51 +0300

    решили вторую задачу

--- Program.cs
+++ Program.cs
@@ -1,23 +1,59 @@
 ﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
 
+// int ReadInt(string str)
+// {
+//     System.Console.Write(str);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// void PrintNumbers(int M, int N)
+// {
+//     if (M > N)
+//         return;
+//     Console.Write(M + " ");
+//     PrintNumbers(M + 1, N);
+// }
+
+// // ----------------------
+// int M = ReadInt("Введите первое значение: ");
+// int N = ReadInt("Введите второе значение: ");
+
+// PrintNumbers(M, N);
+
+// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
 int ReadInt(string str)
 {
-System.Console.Write(str);
-return Convert.ToInt32(Console.ReadLine());
+    System.Console.Write(str);
+    return Convert.ToInt32(Console.ReadLine());
 }
-
-void PrintNumbers(int M, int N)
+// вызов функции Аккермана
+void AkkermanFunction(int M, int N)
 {
-if(M > N) 
-return;
-
-
-Console.Write(M + " ");
-PrintNumbers(M + 1,N);
+    Console.Write(Akkerman(M, N));
 }
 
-// ----------------------
+// функция Аккермана
+int Akkerman(int M, int N)
+{
+    if (M == 0)
+    {
+        return N + 1;
+    }
+    else if (N == 0 && M > 0)
+    {
+        return Akkerman(M - 1, 1);
+    }
+    else
+    {
+        return Akkerman(M - 1, Akkerman(M, N - 1));
+    }
+}
+// _____________________________
 int M = ReadInt("Введите первое значение: ");
 int N = ReadInt("Введите второе значение: ");
 
-PrintNumbers(M , N);
+AkkermanFunction(M, N);
+
